7ea33ac73f4d0c7ae826b23ae1f185e0e68ebd6a
NMONDA <ndegemonda@gmail.com>
2023-05-17 03:18:01 +0300

    create a Squad object

--- src/main/java/ke/co/pookie/Squad.java
+++ src/main/java/ke/co/pookie/Squad.java
@@ -1,4 +1,18 @@
 package ke.co.pookie;
 
+import java.time.LocalDateTime;
+
 public class Squad {
+    private int id;
+    private String squadName;
+    private String cause;
+    private int maxSize;
+    private LocalDateTime createdAt;
+
+    public Squad(int id, String squadName, String cause, int maxSize, LocalDateTime createdAt) {
+        this.id = id;
+        this.squadName = squadName;
+        this.cause = cause;
+        this.maxSize = maxSize;
+        this.createdAt = createdAt;
 }
