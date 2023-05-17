a9927844c43b03b3a734bd009fd0d24bc9b9700d
NMONDA <ndegemonda@gmail.com>
2023-05-17 05:34:45 +0300

    modified dependencies, HeroDao and SquadDao modified

--- src/main/java/ke/co/pookie/dao/HeroDao.java
+++ src/main/java/ke/co/pookie/dao/HeroDao.java
@@ -1,4 +1,30 @@
 package ke.co.pookie.dao;
 
+import ke.co.pookie.Hero;
+import org.sql2o.Sql2o;
+
 public class HeroDao {
+    private final static Sql2o sql2o = Database.getDatabase();
+    public static void add(Hero newHero){
+        public void createHero(Connection sql, Hero hero) {
+            try {
+                sql.createQuery("INSERT INTO heroes (heroname, power, weakness, squadid) VALUES (:name, :power, :weakness, :squadId)", true)
+                        .bind(hero)
+                        .executeUpdate();
+            } catch (Exception e) {
+                throw new RuntimeException("Error Occured", e);
+            }
+        }
+
+        public void deleteHero(Connection sql, int id) {
+            sql.createQuery("DELETE FROM heroes WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+
+        public void updateHero(Connection sql, Hero hero) {
+            sql.createQuery("UPDATE heroes SET heroName = :name, power = :power, weakness = :weakness, squadId = :squadId WHERE id = :id")
+                    .bind(hero)
+                    .executeUpdate();
+        }
 }
--- src/main/java/ke/co/pookie/dao/SquadDao.java
+++ src/main/java/ke/co/pookie/dao/SquadDao.java
@@ -1,4 +1,19 @@
 package ke.co.pookie.dao;
 
+import ke.co.pookie.Squad;
+import org.sql2o.Sql2o;
+
 public class SquadDao {
+    private final static Sql2o sql2o = Database.getDatabase();
+    public static void (Connection sql, Squad squad) {
+        try{
+            String sqlQuery = "INSERT INTO squads (squad name,cause, maxsize, create) VALUES (:squadName, :cause, :maxSize, :createdAt)";
+            return  (int)sql.createQuery(sqlQuery, true)
+                    .bind(squad)
+                    .executeUpdate().getKey();
+
+        }catch (Exception e){
+            throw new RuntimeException("Error Occurred", e);
+        }
+    }
 }
