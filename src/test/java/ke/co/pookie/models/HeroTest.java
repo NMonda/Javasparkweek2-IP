c47657142407a13620497526d25ba872aa850323
NMONDA <ndegemonda@gmail.com>
2023-06-07 11:29:24 +0300

    created test files and added tests

---
+++ src/test/java/ke/co/pookie/models/HeroTest.java
@@ -0,0 +1,84 @@
+package ke.co.pookie.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testFinalize() {
+    }
+
+    @Test
+    void setHero() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void setPower() {
+    }
+
+    @Test
+    void setPower_score() {
+    }
+
+    @Test
+    void setWeakness() {
+    }
+
+    @Test
+    void setWeakness_score() {
+    }
+
+    @Test
+    void setSquad() {
+    }
+
+    @Test
+    void getHero() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void getPower() {
+    }
+
+    @Test
+    void getPower_score() {
+    }
+
+    @Test
+    void getWeakness() {
+    }
+
+    @Test
+    void getWeakness_score() {
+    }
+
+    @Test
+    void getSquad() {
+    }
+}
---
+++ src/test/java/ke/co/pookie/models/SquadTest.java
@@ -0,0 +1,36 @@
+package ke.co.pookie.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadTest {
+
+    @Test
+    void getSquad() {
+    }
+
+    @Test
+    void setSquad() {
+    }
+
+    @Test
+    void getCause() {
+    }
+
+    @Test
+    void setCause() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void setSize() {
+    }
+}
